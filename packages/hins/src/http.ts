aaf09f0881b9a08080afdcd998b5de0fa02e0879
zoy <409626581@qq.com>
2021-02-03 18:02:47 +0800

    fix

--- packages/hins/src/http.ts
+++ packages/hins/src/http.ts
@@ -1,6 +1,14 @@
-import connect from 'connect'
+import connect, { IncomingMessage, NextFunction } from 'connect'
 import http from 'http'
 
 export async function getHttpServer(app: connect.Server) {
   return http.createServer(app)
+}
+
+export function transformRequest(
+  req: IncomingMessage,
+  res: http.ServerResponse,
+  next: NextFunction
+) {
+  console.log(req, res)
 }
--- packages/hins/src/server.ts
+++ packages/hins/src/server.ts
@@ -2,7 +2,7 @@ import compression from 'compression'
 import connect from 'connect'
 import sirv from 'sirv'
 
-import { getHttpServer } from './http'
+import { getHttpServer, transformRequest } from './http'
 
 export async function server() {
   const app = connect()
@@ -11,18 +11,7 @@ export async function server() {
 
   app.use(sirv(__dirname))
 
-  app.use((req, res, next) => {
-    // res.statusCode = 200
-    // res.end('hellow world')
-    console.log(req.url)
-    res.setHeader('Content-Type', 'application/javascript')
-    res.end(`    import abc from './abc'
-    export default () => {
-
-      console.log(1111)
-
-    }`)
-  })
+  app.use(transformRequest)
 
   const server = await getHttpServer(app)
 
